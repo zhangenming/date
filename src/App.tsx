fca13ddd6d2c05a78e9eeb52c052a4db97d78b19
zhangenming <282126346@qq.com>
2022-11-11 14:42:16 +0800

    有被rbs气到

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+// safelist
+// col-span-1
+// col-span-2
+// col-span-3
+// col-span-4
+// col-span-5
+// col-span-6
+// col-span-7
+
 function App({
   datas,
   dates,
@@ -22,16 +31,16 @@ function App({
     selectMonthSet(month => month + n)
   }
   return (
-    <div className="select-none">
+    <div className="mx-30rem select-none ">
       {/* title */}
-      <div className="flex h-[100rem] mx-[30rem] text-size-[34rem] text-color-[#1a1a1a] justify-between items-center">
-        <div className="left" onClick={() => dateHandle(-1)}></div>
-        <div className="text-color-[#1a1a1a]">{`${year}年${month}月`}</div>
-        <div className="right" onClick={() => dateHandle(1)}></div>
+      <div className="flex h-100rem text-size-34rem text-color-[#1a1a1a] justify-between items-stretch children:(flex-1 bg-no-repeat) ">
+        <div className="bg-left" onClick={() => dateHandle(-1)}></div>
+        <div className="text-center self-center">{`${year}年${month}月`}</div>
+        <div className="bg-right" onClick={() => dateHandle(1)}></div>
       </div>
 
       {/* 星期 */}
-      <div className="border-y flex h-[70rem] text-size-[26rem] text-color-[#1a1a1a] items-center children:(w-1/7 text-center) ">
+      <div className="border-y flex h-70rem text-size-26rem text-color-[#1a1a1a] items-center children:(w-1/7 text-center) ">
         <div className="text-color-[#bbbbbf]">日</div>
         <div>一</div>
         <div>二</div>
@@ -42,8 +51,11 @@ function App({
       </div>
 
       {/* 日期 */}
-      <div className="grid children:(flex flex-col items-center justify-evenly text-color-[#bbbbbf] leading-normal) ">
-        {<div className={`placeholder${firstDay}`}></div>}
+      <div className="mt-50rem grid  gap-y-20rem children:(flex flex-col items-center justify-evenly text-color-[#bbbbbf] leading-normal rounded-1/10) ">
+        {/* 占位符 */}
+        <div className={`col-span-${firstDay}`}></div>
+
+        {/*items */}
         {monthDays.map(day => {
           const target = targets.find(target => target.date.slice(-2) === day)
           return (
@@ -55,7 +67,7 @@ function App({
               >
                 {day}
               </div>
-              <div className={`h-[1em] text-size-[20rem] text-color-[#12b5b2]`}>
+              <div className="h-1em text-size-20rem text-color-[#12b5b2]">
                 {target?.adult_amount}
               </div>
             </div>
